d2f9c366f875f313a00ad36aaa32f8449111ab44
Jan Rękorajski <baggins@pld-linux.org>
2020-02-12 07:34:26 +0100

    - fix paths to /usr/lib/rpm in macro files - rel 2

--- attr.php
+++ attr.php
@@ -1,5 +1,5 @@
-%__php_provides		%{_rpmlibdir}/php.prov
+%__php_provides		%{_rpmhome}/php.prov
 # define 'php_req_new' in ~/.rpmmacros to use php version of req finder
-%__php_requires		env PHP_MIN_VERSION=%{?php_min_version} %{_rpmlibdir}/php.req%{?php_req_new:.php}
+%__php_requires		env PHP_MIN_VERSION=%{?php_min_version} %{_rpmhome}/php.req%{?php_req_new:.php}
 %__php_magic		^PHP script.*
 %__php_path		\\.php$
