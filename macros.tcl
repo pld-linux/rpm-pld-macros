ca5c131894c9478432028c4843cbd804f9f32037
Jan Rękorajski <baggins@pld-linux.org>
2021-03-01 20:41:47 +0100

    - fix tcl_version macro expanding to nothing if tclsh is not present

--- macros.tcl
+++ macros.tcl
@@ -2,6 +2,6 @@
 
 %__tclsh	/usr/bin/tclsh
 
-%tcl_version	%{expand:%%global tcl_version %(echo 'puts $tcl_version' | %{__tclsh})}%tcl_version
+%tcl_version	%{expand:%%global tcl_version %(V=$(echo 'puts $tcl_version' | %{__tclsh}); echo ${V:-0})}%tcl_version
 %tcl_sitearch	%{_libdir}/tcl%{tcl_version}
 %tcl_sitelib	%{_datadir}/tcl%{tcl_version}
